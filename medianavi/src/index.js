ac13570a37bb83b4c8fd116d0542696069f4c26d
sorinies <7258136+sorinies@users.noreply.github.com>
2022-10-24 10:41:59 +0900

    블로그 포스트 발행

--- medianavi/src/index.js
+++ medianavi/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga4';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './assets/css/style.css';
@@ -6,6 +7,7 @@ import App from './App';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+ReactGA.initialize("G-144HB93NR5");
 root.render(
   <React.StrictMode>
     <BrowserRouter>
